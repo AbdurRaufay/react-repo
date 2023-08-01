62bbc7f766d72399e3be205d2be758b080c02dd6
abdurrauf <abdurrauf0306@gmail.com>
2023-07-31 23:51:45 -0700

    fl

--- pages/login/[login].js
+++ pages/login/[login].js
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { useLoginUserMutation } from "../../queries/generated";
+import { useLoginUserMutation, useLoginUserWithGoogleMutation } from "../../queries/generated";
 import { useRouter } from "next/router";
 const LoginInput = () => {
     const router=useRouter()
   const { mutateAsync, loading } = useLoginUserMutation();
+  const {loginMutate}=useLoginUserWithGoogleMutation()
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -36,27 +37,64 @@ const LoginInput = () => {
       console.log(error);
     }
   };
-  // const handleGoogleLoginSuccess = async (response) => {
+  const handleSignInWithGoogle = async () => {
+    try {
+      // Create the URL for Google OAuth
+      const googleAuthUrl = "https://accounts.google.com/o/oauth2/v2/auth" +
+        "?client_id=22373809511-mhhg6mh4dviq96s2vra6umd4nkgse1rl.apps.googleusercontent.com" + // Replace with your Google Client ID
+        "&redirect_uri=https://dark-zipper-deer.cyclic.cloud/auth/google/callback" + // Replace with your backend redirect URL
+        "&response_type=code" +
+        "&scope=https://www.googleapis.com/auth/userinfo.email" +
+        "&access_type=offline" +
+        "&prompt=consent";
+  
+      // Open a new pop-up window
+      const width = 500;
+      const height = 600;
+      const left = window.screen.width / 2 - width / 2;
+      const top = window.screen.height / 2 - height / 2;
+      const googleAuthWindow = window.open(
+        googleAuthUrl,
+        "GoogleAuthWindow",
+        `width=${width},height=${height},top=${top},left=${left}`
+      );
+  
+      // Listen for the "message" event from the pop-up window
+      window.addEventListener("message", async (event) => {
+        // Make sure the message is from the pop-up window and contains the access token
+        if (event.source === googleAuthWindow && event.data?.accessToken) {
+          const accessToken = event.data.accessToken;
+          // Send the access token to your backend for verification and user creation/login
+          try {
+            const response = await loginMutate(accessToken);
+            const token = response?.data?.loginUserWithGoogle?.token;
+            const userId = response?.data?.loginUserWithGoogle?.userId;
+            if (token && userId) {
+              localStorage.setItem("token", token);
+              localStorage.setItem("userId", userId);
+              router.push("/");
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // const handleSignInWithGoogle = async () => {
   //   try {
-  //     // Send the Google ID token to your backend for verification and user creation/login
-  //     const idToken = response?.tokenId; // Get the Google ID token from the response
-  //     const response = await fetch("https://dark-zipper-deer.cyclic.cloud/auth/google/callbackL", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ idToken }),
+  //     const response = await fetch("https://dark-zipper-deer.cyclic.cloud/auth/google/callback", {
+  //       method: "GET",
+  //       mode: "cors",
+  //       credentials: "include",
   //     });
-
+  //       console.log(response.url,"clg")
   //     if (response.ok) {
-  //       const data = await response.json();
-  //       const token = data.token;
-  //       const userId = data.userId;
-  //       if (token && userId) {
-  //         localStorage.setItem("token", token);
-  //         localStorage.setItem("userId", userId);
-  //         router.push("/");
-  //       }
+  //       // Redirect to the Google Sign-In page
+  //       window.location.href = response.url;
+  //       console.log(response,"res")
   //     } else {
   //       console.log("Error occurred during Google Sign-In");
   //     }
@@ -64,27 +102,6 @@ const LoginInput = () => {
   //     console.log(error);
   //   }
   // };
-  const handleSignInWithGoogle = async () => {
-    try {
-      // Call the backend server endpoint for Google Sign-In
-      // const response = await fetch("https://prismatic-sunshine-ec4f13.netlify.app/auth/google/callback", {
-      const response = await fetch("https://dark-zipper-deer.cyclic.cloud/auth/google/callback", {
-        method: "GET",
-        mode: "cors",
-        credentials: "include",
-      });
-        console.log(response.url,"clg")
-      if (response.ok) {
-        // Redirect to the Google Sign-In page
-        window.location.href = response.url;
-        console.log(response,"res")
-      } else {
-        console.log("Error occurred during Google Sign-In");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <div>
       <form onSubmit={handleSubmitForm}>
